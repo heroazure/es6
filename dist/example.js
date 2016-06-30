a0a8e28a7c9010f9af4947f3d9e2b336fea3749d
heroazure <1147024669@qq.com>
2016-06-30 08:17:43 +0800

    'dd'

---
+++ dist/example.js
@@ -0,0 +1,13 @@
+"use strict";
+
+/**
+ * Created by 11470 on 2016/6/30.
+ */
+var arry = [1, 2, 3];
+var n = arry.map(function (item) {
+  return item + 3;
+});
+console.log(n);
+var kk = { a: 1 };
+var t = Object.assign({}, kk);
+//# sourceMappingURL=example.js.map
---
+++ src/example.js
@@ -0,0 +1,8 @@
+/**
+ * Created by 11470 on 2016/6/30.
+ */
+var arry=[1,2,3]
+var n=arry.map(item=>item+3)
+console.log(n)
+var kk={a:1}
+var t=Object.assign({},kk)
